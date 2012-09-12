20e42597505e8e2e95755e1c3403fa6edad5f482
flaviozantut <webmaster@flaviozantut.com>
2012-09-12 19:50:30 -0300

    change docs

--- docs.sh
+++ docs.sh
@@ -1,2 +1,2 @@
 #phpdoc -d . -t docs -i libraries/Imagine/  --title "thumburl"
-apigen --source . --destination docs2 --exclude "*libraries/Imagine/*"
+apigen --source . --destination docs --exclude "*libraries/Imagine/*" --exclude "*docs*" --template-config /usr/share/php/data/ApiGen/templates/bootstrap/config.neon --title thumburl
